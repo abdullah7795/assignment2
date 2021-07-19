4a0fb9e072a7a5d312548d1089dfedfd0c3e634f
mohammed abdullah <abdullah7795131028@gmail.com>
2021-07-19 08:46:30 -0700

    Updates

---
+++ precache-manifest.70964261e7831be0a1d8bc62e39df418.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c966dd4e2a18bb3962f8c8156ce3bfe6",
+    "url": "/assignment2/index.html"
+  },
+  {
+    "revision": "3d04358cb60c3399359e",
+    "url": "/assignment2/static/css/main.c8246e2a.chunk.css"
+  },
+  {
+    "revision": "233138de32671055329f",
+    "url": "/assignment2/static/js/2.ba41c62e.chunk.js"
+  },
+  {
+    "revision": "3d04358cb60c3399359e",
+    "url": "/assignment2/static/js/main.94d89d17.chunk.js"
+  },
+  {
+    "revision": "3a5d73dd7d41a1db7a1d",
+    "url": "/assignment2/static/js/runtime-main.80bf43ca.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/assignment2/static/media/logo.25bf045c.svg"
+  }
+]);
